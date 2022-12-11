84c0b8e7e62580771ac250190d2b79b3e6a234fa
officialzoka <golldenhouse@gmail.com>
2022-12-11 15:21:58 +0200

    animation duration

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
-    <NuxtLoadingIndicator color="#4338ca" :duration="Number(4000)" />
+    <NuxtLoadingIndicator color="#4338ca" :duration="Number(2000)" />
     <NuxtPage />
 </template>
 <style>
 .layout-enter-active,
 .layout-leave-active {
-    transition: all 0.4s;
+    transition: all 0.2s;
 }
 .layout-enter-from,
 .layout-leave-to {
